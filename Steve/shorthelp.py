90878883d1570729ccb52dc09e2c09a6b7161337
Steven Davies <stevenstdc@gmail.com>
2018-11-03 14:56:51 +0000

    Code to ues later

---
+++ Steve/shorthelp.py
@@ -0,0 +1,8 @@
+#prints denary equivilant
+print(int("0101001", 2))
+#prints hex equivilant
+print(hex("01010101", 2))
+#returns ascii value
+print(ord("A"))
+#prints character from ascii
+print(chr(93))
