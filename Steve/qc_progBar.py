1af1c68838794dfc84b5cef046c6c351d76e816e
Steven Davies-Cortes <43970645+StivDC@users.noreply.github.com>
2019-09-30 14:55:21 +0100

    Update qc_progBar.py

--- Steve/qc_progBar.py
+++ Steve/qc_progBar.py
@@ -9,3 +9,16 @@ prog = float(input()) # Current progress of bar
 r = a * math.ceil(prog*l) + b * math.floor(l*(1-prog))
 
 print(r)
+
+###JAVASCRIPT VERSION
+
+#const A = readline();
+#const B = readline();
+#const L = parseInt(readline());
+#const prog = parseFloat(readline());
+
+#// Write an action using console.log()
+#// To debug: console.error('Debug messages...');
+
+
+#console.log(A.repeat(prog*L+.9999|0)+B.repeat(L-(prog*L+.9999|0)));
