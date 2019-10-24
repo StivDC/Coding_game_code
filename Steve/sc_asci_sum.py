c4bada22ab7a420e579dab706f20be0fedea77f1
Steven Davies-Cortes <43970645+StivDC@users.noreply.github.com>
2019-10-24 13:14:17 +0100

    Update sc_asci_sum.py

--- Steve/sc_asci_sum.py
+++ Steve/sc_asci_sum.py
@@ -1,3 +1,5 @@
 #my first proper gen expression
 #prints sum of ascii for each character in an input
 print(sum(ord(c)for c in input()))
+# Gathers binary equivilant in 7bits and strips out the 0b
+to_bin = lambda c: bin(ord(c))[2:].zfill(7) 
