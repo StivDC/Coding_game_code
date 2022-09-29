8f0880d7de377c0fed8b511535d83f574ee40468
Steven Davies-Cortes <43970645+StivDC@users.noreply.github.com>
2022-09-29 14:49:08 +0100

    Return the key with the max val in a dictionary
    This is something I normally come back to when using dictionaries, finally found a solution which is useful and easy to understand. Saving for future use.

---
+++ Steve/qc_keywithmaxval.py
@@ -0,0 +1,6 @@
+def keywithmaxval(d):
+     """ a) create a list of the dict's keys and values; 
+         b) return the key with the max value"""  
+     v = list(d.values())
+     k = list(d.keys())
+     return k[v.index(max(v))]
